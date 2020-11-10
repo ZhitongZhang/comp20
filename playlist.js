1b0652ae62fd1cfbec3a517ebd145f90989ac081
Zhitong Zhang <zhitong.zhang@tufts.edu>
2020-11-09 22:28:15 -0500

    add

---
+++ playlist.js
@@ -0,0 +1,79 @@
+playlist = [
+    {
+      "artist": ["DJ Khaled", "Drake"],
+      "title": "POPSTAR",
+      "album": "POPSTAR",
+      "year": "2020",
+      "genre": ["Pop"],
+    },
+  
+    {
+      "artist": ["Luke Combs"],
+      "title": "Forever After All",
+      "album": "What You see Ain't Always What You Get",
+      "year": "2020",
+      "genre": ["Pop"],
+    },
+    {
+      "artist": ["Lauv", "Conan Gray"],
+      "title": "Fake",
+      "album": "Fake",
+      "year": "2020",
+      "genre": ["Pop"],
+    },
+    {
+      "artist": ["Lil Nas X"],
+      "title": "Old Town Road",
+      "album": "Old Town Road",
+      "year": "2019",
+      "genre": ["Rap" , "Country"],
+    },
+  
+    {
+      "artist": ["Russell Dickerson"],
+      "title": "Love You Like I Used To",
+      "album": "Love you Like I Used To",
+      "year": "2020",
+      "genre": ["Country"],
+    },
+  
+    {
+      "artist": ["Billy Currington"],
+      "title": "Seaside",
+      "album": "Seaside",
+      "year": "2020",
+      "genre": ["Country"],
+    },
+  
+    {
+      "artist": ["Lindsey Ell"],
+      "title": "wAnt me back",
+      "album": "wAnt me back",
+      "year": "2020",
+      "genre": ["Country"],
+    },
+  
+    {
+      "artist": ["Polo G"],
+      "title": "Martin & Gina",
+      "album": "THE GOAT",
+      "year": "2020",
+      "genre": ["Rap"],
+    },
+  
+    {
+      "artist": ["CJ"],
+      "title": "Whoopty",
+      "album": "Whoopty",
+      "year": "2020",
+      "genre": ["Rap"],
+    },
+  
+    {
+      "artist": ["Trippie Redd"],
+      "title": "Sleepy Hollow",
+      "album": "Sleepy Hollow",
+      "year": "2020",
+      "genre": ["Rap"],
+    }
+  ]
